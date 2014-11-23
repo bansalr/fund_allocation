6c8654f9c11e7d8b7cdabc9f8ad24d8545b7fb23
Ritesh Bansal <rbansal@gmail.com>
2014-11-23 01:26:42 -0500

    moving to ipython

--- optimizer.py
+++ optimizer.py
@@ -5,6 +5,14 @@ import pymprog
 from locale import *
 from pylab import *
 import textwrap
+import pandas.io.data as web
+import pandas as pd
+from datetime import datetime
+import os
+import scipy
+from matplotlib import pyplot as PLT
+
+
 setlocale(LC_NUMERIC, '')
 
 def loadFundDB(db_file):
@@ -19,18 +27,50 @@ def loadFundDB(db_file):
 	return fundsDict
 
 
+def get_data(fundsDict):
+	all_tickers = sorted(fundsDict.keys())
+	startDate     = datetime(2014,1,3) ### picked
+	endDate       = datetime.now()
+	all_data      = web.DataReader ( all_tickers, 'yahoo', startDate, endDate)
+	ac = all_data['Adj Close']
+	acr = ac / ac.shift(1) - 1
+	index  = (1+acr).cumprod()
+	return index
 
-def pie_chart_portfolio ( fundLabels, fundAmounts, fee):
+def pie_chart_portfolio ( fundLabels, fundAmounts, fee, fig):
 	#print(fundLabels)
 	#print(fundAmounts)
 	# make a square figure and axes
-	figure(1, figsize=(8,8))
-	ax = axes([0.1, 0.1, 0.8, 0.8])
+
+	ax1 = fig.add_subplot(211)
+	#ax = axes([0.1, 0.1, 0.8, 0.8])
 	title_str = 'Satisfying Allocation, Fee: %2.1f bps' % ( fee *1e4)
 	explode = [0.03] * len(fundLabels)
 	pie(fundAmounts, explode=explode, labels=fundLabels, autopct='%1.1f%%', shadow=True, startangle=90)
 	title(title_str, bbox={'facecolor':'0.8', 'pad':10})
-	show()
+
+
+def sim_port(fundTickers, fundDollars,fig):
+	end_date = '2014-11-21'
+	series = {}
+	print(fundTickers)
+	for ticker,dollarAmt in zip(fundTickers,fundDollars):
+		s = pd.Series(index[ticker])
+		s = s.replace('NaN',1)
+		series[ticker] = s.multiply(dollarAmt)
+
+	tkr0 = fundTickers[0]
+	s0 = series[tkr0]
+	dates = s0.index.get_values()
+	sum = pd.Series(scipy.zeros(len(dates)), dates)
+	for ticker in fundTickers:
+		sum = sum.add(series[ticker],fill_value=0)
+
+	sum['2014-01-03'] = sum['2014-01-06']
+	ax2 = fig.add_subplot(212)
+	ax2.plot(sum.index.get_values(),pd.DataFrame(sum))
+	PLT.show()
+	return sum
 
 
 ######################################################################
@@ -50,16 +90,24 @@ def setupLP(minPurchaseFlag = False, geoFlag = False, assetFlag = False ):
 	def pretty_print_portfolio():
 		fundLabels = list()
 		fundWeights = list()
+		fundTickers = list()
+		fundDollars = list()
 		for i in cid:
 			if fundAmounts[i].primal > 0:
 				l = textwrap.wrap(fundList[i]["Name"]+"\n"+fundList[i]["Ticker"], 16)
+				fundTickers.append(fundList[i]["Ticker"])
 				fundLabels.append("\n".join(l))
+				fundDollars.append(fundAmounts[i].primal)
 				fundWeights.append(fundAmounts[i].primal/dollars.primal)
 				print("%g %s : %s : %g : binary: %g" %
 					  (i, fundList[i]["Ticker"], fundList[i]["Name"], fundAmounts[i].primal, fundMinBinary[i].primal)
 				  )
-		pie_chart_portfolio(fundLabels, fundWeights, (portfolio.vobj()/dollars.primal))
+
+		fig = PLT.figure()
+		pie_chart_portfolio(fundLabels, fundWeights, (portfolio.vobj()/dollars.primal), fig)
+		sum = sim_port(fundTickers,fundDollars, fig)
 		print("Total Fees Are: %g" % (portfolio.vobj()/dollars.primal))
+		return sum
 
 	for index,k in enumerate(sorted(fundsDict.keys()), start=0):
 		f = fundsDict[k]
@@ -161,10 +209,24 @@ def setupLP(minPurchaseFlag = False, geoFlag = False, assetFlag = False ):
 	print "Solve status: ", portfolio.status()
 
 	#pretty_print_portfolio ( fundAmounts, portfolio, dollars, cid, fundList )
-	pretty_print_portfolio ()
+	return pretty_print_portfolio ()
 	### main
+	#return fundTickers,fundDollars
 
-code.interact(local=locals())
+
+#code.interact(local=locals())
 fundsDict= loadFundDB("./db/vanguard_db.psv")
 
+index_file_name ="df_index.save"
+if os.path.isfile(index_file_name):
+	index = pd.load(index_file_name)
+else:
+	index = get_data(fundsDict)
+
 code.interact(local=locals())
+
+# fT,fD = setupLP(False,False,False)
+
+# fT,fD = setupLP(True,False,False)
+# fT,fD = setupLP(True,True, False)
+# fT,fD = setupLP(True,True,True)
